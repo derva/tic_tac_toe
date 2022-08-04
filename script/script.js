60b67204fb6193b7f5c11c5043c98a54d1efeae5
derva <dervishalilcevic@gmail.com>
2022-08-04 23:53:28 +0200

    Add restart button function

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@ let fields = document.querySelectorAll('.field');
 let fieldsArr = Array.from(fields);
 let index,mark = 'X';
 let counter = 0;
+let restartButton = document.getElementById('restartButton');
+
 
 let game = ( () => {
     let gameBoard = [];
@@ -39,20 +41,29 @@ let game = ( () => {
 })();
 
 let displayController = ( ()=> {
+    let restart = () => {
+        
+    }
+    let winner = (name) => {
+        alert('Player ' + name + ' win this game!');
+    }
     let checkRows = () =>{
         if(game.gameBoard[0] === game.gameBoard[1] && game.gameBoard[1] === game.gameBoard[2]){
             if(!(game.gameBoard[0] == undefined &&game.gameBoard[1] == undefined && game.gameBoard[2] == undefined)){
                 console.log('winner in row1, and winner is: ' + game.gameBoard[0]);
+                winner(game.gameBoard[0]);
             }
         }
         else if(game.gameBoard[3] === game.gameBoard[4] && game.gameBoard[4] === game.gameBoard[5]){
             if(!(game.gameBoard[3] == undefined &&game.gameBoard[4] == undefined && game.gameBoard[5] == undefined)){
                 console.log('winner in row2, and winner is: ' + game.gameBoard[3]);
+                winner(game.gameBoard[3]);
             }
         }
         else if(game.gameBoard[6] === game.gameBoard[7] && game.gameBoard[7] === game.gameBoard[8]){
             if(!(game.gameBoard[6] == undefined &&game.gameBoard[7] == undefined && game.gameBoard[8] == undefined)){
                 console.log('winner in row3, and winner is: ' + game.gameBoard[6]);
+                winner(game.gameBoard[6]);
             }
         }
     }
@@ -61,16 +72,19 @@ let displayController = ( ()=> {
         if(game.gameBoard[0] === game.gameBoard[3] && game.gameBoard[3] === game.gameBoard[6]){
             if(!(game.gameBoard[0] == undefined &&game.gameBoard[3] == undefined && game.gameBoard[6] == undefined)){
                 console.log('winner in column1, and winner is: ' + game.gameBoard[0]);
+                winner(game.gameBoard[0]);
             }
         }
         else if(game.gameBoard[1] === game.gameBoard[4] && game.gameBoard[4] === game.gameBoard[7]){
             if(!(game.gameBoard[1] == undefined &&game.gameBoard[4] == undefined && game.gameBoard[7] == undefined)){
                 console.log('winner in column2, and winner is: ' + game.gameBoard[4]);
+                winner(game.gameBoard[4]);
             }
         }
         else if(game.gameBoard[2] === game.gameBoard[5] && game.gameBoard[5] === game.gameBoard[8]){
             if(!(game.gameBoard[2] == undefined &&game.gameBoard[5] == undefined && game.gameBoard[8] == undefined)){
                 console.log('winner in column3, and winner is: ' + game.gameBoard[5]);
+                winner(game.gameBoard[5]);
             }
         }
     }
@@ -78,11 +92,13 @@ let displayController = ( ()=> {
         if(game.gameBoard[0] === game.gameBoard[4] && game.gameBoard[4] === game.gameBoard[8]){
             if(!(game.gameBoard[0] == undefined && game.gameBoard[4] == undefined && game.gameBoard[8] == undefined)){
                 console.log('winner in digaonal1, and winner is: ' + game.gameBoard[0]);
+                winner(game.gameBoard[0]);
             }
         }
         else if(game.gameBoard[2] === game.gameBoard[4] && game.gameBoard[4] === game.gameBoard[6]){
             if(!(game.gameBoard[2] == undefined && game.gameBoard[4] == undefined && game.gameBoard[6] == undefined)){
                 console.log('winner in diagonal2, and winner is: ' + game.gameBoard[4]);
+                winner(game.gameBoard[4]);
             }
         }
     }
@@ -92,7 +108,7 @@ let displayController = ( ()=> {
         checkDiagonals();
         
     }
-    return {checkWinner};
+    return {checkWinner,restart};
 })();
 
 let Player = (name) =>{
@@ -100,6 +116,16 @@ let Player = (name) =>{
     return {getName}
 }
 
+let id = Array.from(document.querySelectorAll('.field'));
+
+restartButton.addEventListener('click', () => {
+    for(i = 0; i < 9; i++){
+        
+        game.gameBoard[i] = undefined;
+        id[i].innerText = '';
+        // console.log('i: ' + i);
+    }
+});
 // fields.forEach( (i) => {
 //     i.addEventListener('click', ()=>{
 //         console.log('index: ' + i.getAttribute('data-position'));
