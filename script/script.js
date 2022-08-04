f6070371ff8929e71d84ce34e51eba1198fd32e1
derva <dervishalilcevic@gmail.com>
2022-08-04 21:43:36 +0200

    Add checkWinner() function

--- script/script.js
+++ script/script.js
@@ -28,7 +28,9 @@ let game = ( () => {
                     gameBoard[index-1] = mark;
                     i.innerText = mark;
                     
-                    display();
+                    // display();
+                    displayController.checkWinner();
+                    // displayController.checkColumns();
                 }
             })
         })
@@ -37,8 +39,60 @@ let game = ( () => {
 })();
 
 let displayController = ( ()=> {
-    
-    return {};
+    let checkRows = () =>{
+        if(game.gameBoard[0] === game.gameBoard[1] && game.gameBoard[1] === game.gameBoard[2]){
+            if(!(game.gameBoard[0] == undefined &&game.gameBoard[1] == undefined && game.gameBoard[2] == undefined)){
+                console.log('winner in row1, and winner is: ' + game.gameBoard[0]);
+            }
+        }
+        else if(game.gameBoard[3] === game.gameBoard[4] && game.gameBoard[4] === game.gameBoard[5]){
+            if(!(game.gameBoard[3] == undefined &&game.gameBoard[4] == undefined && game.gameBoard[5] == undefined)){
+                console.log('winner in row2, and winner is: ' + game.gameBoard[3]);
+            }
+        }
+        else if(game.gameBoard[6] === game.gameBoard[7] && game.gameBoard[7] === game.gameBoard[8]){
+            if(!(game.gameBoard[6] == undefined &&game.gameBoard[7] == undefined && game.gameBoard[8] == undefined)){
+                console.log('winner in row3, and winner is: ' + game.gameBoard[6]);
+            }
+        }
+    }
+
+    let checkColumns = () =>{
+        if(game.gameBoard[0] === game.gameBoard[3] && game.gameBoard[3] === game.gameBoard[6]){
+            if(!(game.gameBoard[0] == undefined &&game.gameBoard[3] == undefined && game.gameBoard[6] == undefined)){
+                console.log('winner in column1, and winner is: ' + game.gameBoard[0]);
+            }
+        }
+        else if(game.gameBoard[1] === game.gameBoard[4] && game.gameBoard[4] === game.gameBoard[7]){
+            if(!(game.gameBoard[1] == undefined &&game.gameBoard[4] == undefined && game.gameBoard[7] == undefined)){
+                console.log('winner in column2, and winner is: ' + game.gameBoard[4]);
+            }
+        }
+        else if(game.gameBoard[2] === game.gameBoard[5] && game.gameBoard[5] === game.gameBoard[8]){
+            if(!(game.gameBoard[2] == undefined &&game.gameBoard[5] == undefined && game.gameBoard[8] == undefined)){
+                console.log('winner in column3, and winner is: ' + game.gameBoard[5]);
+            }
+        }
+    }
+    let checkDiagonals = () =>{
+        if(game.gameBoard[0] === game.gameBoard[4] && game.gameBoard[4] === game.gameBoard[8]){
+            if(!(game.gameBoard[0] == undefined && game.gameBoard[4] == undefined && game.gameBoard[8] == undefined)){
+                console.log('winner in digaonal1, and winner is: ' + game.gameBoard[0]);
+            }
+        }
+        else if(game.gameBoard[2] === game.gameBoard[4] && game.gameBoard[4] === game.gameBoard[6]){
+            if(!(game.gameBoard[2] == undefined && game.gameBoard[4] == undefined && game.gameBoard[6] == undefined)){
+                console.log('winner in diagonal2, and winner is: ' + game.gameBoard[4]);
+            }
+        }
+    }
+    let checkWinner = () => {
+        checkRows();
+        checkColumns();
+        checkDiagonals();
+        
+    }
+    return {checkWinner};
 })();
 
 let Player = (name) =>{
